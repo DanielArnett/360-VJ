aaf756fed6dc15c92a1275811960f6926f1ee356
Amaury Hazan <amaury@billaboop.com>
2015-03-18 17:30:31 +0100

    Included changes by Lynn Jarvis: v 1.002

--- ShaderMaker.cpp
+++ ShaderMaker.cpp
@@ -10,8 +10,11 @@
 //		26.02.15	Changes for FFGL 1.6
 //					change DWORD to FFResult
 //					remove "Virtual" from destructor definition
-//					Changes  for port to OSX - see 
+//					Changes  for port to OSX
 //					Version 1.001
+//		06.03.15	Provided for revised SharedToy spec with mainImage instead of main
+//                  See ShaderToy example 4
+//					Version 1.002
 //		------------------------------------------------------------
 //
 //		Copyright (C) 2015. Lynn Jarvis, Leading Edge. Pty. Ltd.
@@ -63,7 +66,7 @@ static CFFGLPluginInfo PluginInfo (
 	1,						   			// API major version number 													
 	006,								// API minor version number	
 	1,									// *** Plugin major version number
-	001,								// *** Plugin minor version number
+	002,								// *** Plugin minor version number
 	FF_EFFECT,							// Plugin type is always an effect
 	"Wraps ShaderToy and GLSLSandbox shaders into a FFGL plugin", // *** Plugin description - you can expand on this
 	"by Author - website.com"			// *** About - use your own name and details
@@ -87,8 +90,7 @@ void main()
 // which uses the Stringizing operator (#) (https://msdn.microsoft.com/en-us/library/7e3a913x.aspx)
 // This converts the shader code into a string which is then used by the shader compiler on load of the plugin.
 // There are some limitations of using the stringizing operator in this way because it depends on the "#" symbol,
-// e.g. #( .. code ), Therefore there cannot be any # characters in the code itself. Also there cannot be any
-// commas in the code.
+// e.g. #( .. code ), Therefore there cannot be any # characters in the code itself.
 //
 // For example it is common to see :
 //
@@ -119,6 +121,8 @@ char *fragmentShaderCode = STRINGIFY (
 
 }*/
 
+
+/*
 //
 // Shadertoy example 1
 //
@@ -186,6 +190,8 @@ void main()
     gl_FragColor= vec4(color,1.)+vec4(0.1,0.2,0.5,1.0)*(t*0.025);
 
 }
+*/
+
 
 /*
 //
@@ -219,7 +225,7 @@ void main(void)
 */
 
 
-
+/*
 //
 // Shadertoy example 3
 //
@@ -372,6 +378,123 @@ void main(void)
     gl_FragColor = vec4(col,1.0);
 }
 */
+
+//
+// Shadertoy example 4
+//
+// LJ - Example of revised specification using "mainImage" instead of "main"
+// A fix can be made to include a main function right here, but it is included
+// before compilation in "LoadShader" to be consistent with "ShaderLoader"
+//
+// https://www.shadertoy.com/view/ldl3W8#
+//
+// Voronoi - distances
+//
+// Created by inigo quilez - iq/2013
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+
+// I've not seen anybody out there computing correct cell interior distances for Voronoi
+// patterns yet. That's why they cannot shade the cell interior correctly, and why you've
+// never seen cell boundaries rendered correctly. 
+
+// However, here's how you do mathematically correct distances (note the equidistant and non
+// degenerated grey isolines inside the cells) and hence edges (in yellow):
+
+// http://www.iquilezles.org/www/articles/voronoilines/voronoilines.htm
+
+// LJ - must avoid hash charaters in the code when using stringify. They are OK in comments
+// #define ANIMATE
+bool bAnimate = true;
+
+vec2 hash2( vec2 p )
+{
+    // texture based white noise
+    // return texture2D( iChannel0, (p+0.5)/256.0, -100.0 ).xy;
+	
+    // procedural white noise	
+    return fract(sin(vec2(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5, 183.3))))*43758.5453);
+}
+
+vec3 voronoi( in vec2 x )
+{
+    vec2 n = floor(x);
+    vec2 f = fract(x);
+
+    //----------------------------------
+    // first pass: regular voronoi
+    //----------------------------------
+	// LJ - stringify does not like the comma here !
+	// Declare variables separately
+	// vec2 mg, mr;
+	vec2 mg;
+	vec2 mr;
+
+    float md = 8.0;
+
+    for( int j=-1; j<=1; j++ ) {
+        for( int i=-1; i<=1; i++ ) {
+            vec2 g = vec2(float(i),float(j));
+            vec2 o = hash2( n + g );
+			// LJ avoid the hash character here
+			// #ifdef ANIMATE
+			if(bAnimate)
+			    o = 0.5 + 0.5*sin( iGlobalTime + 6.2831*o );
+			// #endif
+            vec2 r = g + o - f;
+            float d = dot(r,r);
+
+            if( d<md ) {
+                md = d;
+                mr = r;
+                mg = g;
+            }
+        }
+	}
+
+    //----------------------------------
+    // second pass: distance to borders
+    //----------------------------------
+    md = 8.0;
+    for( int j=-2; j<=2; j++ ) {
+        for( int i=-2; i<=2; i++ ) {
+            vec2 g = mg + vec2(float(i),float(j));
+            vec2 o = hash2( n + g );
+			// LJ avoid the hash character here
+			// #ifdef ANIMATE
+			if(bAnimate)
+				o = 0.5 + 0.5*sin( iGlobalTime + 6.2831*o );
+			// #endif
+            vec2 r = g + o - f;
+
+            if( dot(mr-r,mr-r)>0.00001 )
+                md = min( md, dot( 0.5*(mr+r), normalize(r-mr) ) );
+        }
+	}
+
+    return vec3( md, mr );
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 p = fragCoord.xy/iResolution.xx;
+
+    vec3 c = voronoi( 8.0*p );
+
+    // isolines
+    vec3 col = c.x*(0.5 + 0.5*sin(64.0*c.x))*vec3(1.0);
+    
+    // borders	
+    col = mix( vec3(1.0,0.6,0.0), col, smoothstep( 0.04, 0.07, c.x ) );
+    
+    // feature points
+    float dd = length( c.yz );
+    col = mix( vec3(1.0,0.6,0.1), col, smoothstep( 0.0, 0.12, dd) );
+    col += vec3(1.0,0.6,0.1)*(1.0-smoothstep( 0.0, 0.04, dd));
+
+    fragColor = vec4(col,1.0);
+}
+
+
 
 /*
 //
@@ -533,7 +656,7 @@ ShaderMaker::ShaderMaker():CFreeFrameGLPlugin()
 	FILE* pCout; // should really be freed on exit 
 	AllocConsole();
 	freopen_s(&pCout, "CONOUT$", "w", stdout); 
-	printf("Shader Maker Vers 1.000\n");
+	printf("Shader Maker Vers 1.002\n");
 	printf("GLSL version [%s]\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 	*/
 
@@ -1186,7 +1309,22 @@ bool ShaderMaker::LoadShader(std::string shaderString) {
 			stoyUniforms = uniforms;
 			stoyUniforms += extraUniforms;
 			stoyUniforms += shaderString; // add the rest of the shared content
-			shaderString = stoyUniforms;
+
+			// It might be a revised ShaderToy file with "mainImage" instead of "main"
+			if(strstr(shaderString.c_str(), "void mainImage") != 0) {
+				//
+				// If it is a revised spec ShaderToy file, add a fix at the end for GLSL compatibility
+				//
+				// Credit Eric Newman 
+				// http://magicmusicvisuals.com/forums/viewtopic.php?f=2&t=196
+				//
+				static char *stoyMainFunction = { "void main(void) {\n"
+												  "    mainImage(gl_FragColor, gl_FragCoord.xy);\n"
+												  "}\n" };
+				stoyUniforms += stoyMainFunction;
+			}
+
+			shaderString = stoyUniforms; // the final string
 		}
 	
 		// initialize gl shader

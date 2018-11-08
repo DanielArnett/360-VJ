60dc4718dece9d044fad808018f42609f005f7d7
Daniel Arnett <arnett.daniel@gmail.com>
2018-11-08 18:25:05 -0500

    EquiToFisheye now working with FFGL1.5

--- source/EquiToFisheye/EquiToFisheye.cpp
+++ source/EquiToFisheye/EquiToFisheye.cpp
@@ -1,48 +1,43 @@
-#include "EquiToFisheye.h"
-#include <ffgl/FFGLLib.h>
-#include <ffglex/FFGLScopedShaderBinding.h>
-#include <ffglex/FFGLScopedSamplerActivation.h>
-#include <ffglex/FFGLScopedTextureBinding.h>
-using namespace ffglex;
+#include <FFGL.h>
+#include <FFGLLib.h>
 
-//#define FFPARAM_Roll ( 0 )
-//#define FFPARAM_Pitch ( 1 )
-//#define FFPARAM_Yaw ( 2 )
+#include "EquiToFisheye.h"
+#include "../../lib/ffgl/utilities/utilities.h"
+
+//#define FFPARAM_BrightnessR  (0)
+//#define FFPARAM_BrightnessG	 (1)
+//#define FFPARAM_BrightnessB	 (2)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Plugin information
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static CFFGLPluginInfo PluginInfo(
-	PluginFactory< EquiToFisheye >,// Create method
-	"EQ2F",                       // Plugin unique ID of maximum length 4.
-	"360 to Fisheye",		 	  // Plugin name
-	2,                            // API major version number
-	1,                            // API minor version number
-	1,                            // Plugin major version number
-	0,                            // Plugin minor version number
-	FF_EFFECT,                    // Plugin type
+	EquiToFisheye::CreateInstance,		// Create method
+	"EQ2F",								// Plugin unique ID
+	"360 To Fisheye",					// Plugin name
+	1,						   			// API major version number 													
+	500,								// API minor version number
+	1,									// Plugin major version number
+	000,								// Plugin minor version number
+	FF_EFFECT,							// Plugin type
 	"Convert 360 Videos into Fisheye Videos",	  // Plugin description
 	"Written by Daniel Arnett, go to https://github.com/DanielArnett/360-VJ/releases for more detail."      // About
 );
 
-static const char vertexShaderCode[] = R"(#version 410 core
-uniform vec2 MaxUV;
-
-layout( location = 0 ) in vec4 vPosition;
-layout( location = 1 ) in vec2 vUV;
-
-out vec2 uv;
-
-void main()
+static const std::string vertexShaderCode = STRINGIFY(
+	void main()
 {
-	gl_Position = vPosition;
-	uv = vUV * MaxUV;
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	gl_TexCoord[0] = gl_MultiTexCoord0;
+	gl_FrontColor = gl_Color;
 }
-)";
+);
 
-static const char fragmentShaderCode[] = R"(#version 410 core
-uniform sampler2D InputTexture;
 
-in vec2 uv;
+static const std::string fragmentShaderCode = STRINGIFY(
+	uniform sampler2D InputTexture;
 
-out vec4 fragColor;
 float PI = 3.14159265359;
 
 vec2 getLatLonFromFisheyeUv(vec2 uv, float r)
@@ -82,17 +77,19 @@ vec2 pointToLatLon(vec3 p)
 	latLon.x = asin(p.z);
 	latLon.y = -acos(p.x / cos(latLon.x));
 	// Rotate by a quarter turn to align the fisheye image with the center of the 360 image
-	latLon.y += PI/2.0;
+	latLon.y += PI / 2.0;
 	return latLon;
 }
 void main()
 {
+	vec2 uv = gl_TexCoord[0].xy;
+
 	vec2 pos = 2.0 * uv - 1.0;
 	float r = distance(vec2(0.0, 0.0), pos);
 	// Don't bother with pixels outside of the fisheye circle
 	if (1.0 < r) {
 		// Return a transparent pixel
-		fragColor = vec4(0.0,0.0,0.0,0.0);
+		gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
 		return;
 	}
 	vec3 p;
@@ -100,8 +97,6 @@ void main()
 	// Get the latitude and longitude for the destination pixel.
 	vec2 latLon = getLatLonFromFisheyeUv(pos, r);
 	p = latLonToPoint(latLon);
-
-	// TODO align my equirectangular coordinate system with the fisheye coordinate system. Here's a temporary fix.
 	p.z = p.y;
 	p.y = p.z;
 
@@ -109,133 +104,178 @@ void main()
 	latLon = pointToLatLon(p);
 
 	sourcePixel.x = (latLon.y + PI) / (2.0 * PI);
-	sourcePixel.y = (latLon.x + PI/2.0) / PI;
+	sourcePixel.y = (latLon.x + PI / 2.0) / PI;
 
-	fragColor = texture(InputTexture, sourcePixel);
+	gl_FragColor = texture2D(InputTexture, sourcePixel);
 }
-)";
+);
 
-EquiToFisheye::EquiToFisheye() :
-	maxUVLocation( -1 ),
-	fieldOfViewLocation( -1 ),
-	pitch( 0.5f ),
-	yaw( 0.5f ),
-	roll( 0.5f )
+EquiToFisheye::EquiToFisheye()
+	:CFreeFrameGLPlugin(),
+	m_initResources(1),
+	m_inputTextureLocation(-1)//,
+	//m_BrightnessLocation(-1)
 {
 	// Input properties
-	SetMinInputs( 1 );
-	SetMaxInputs( 1 );
+	SetMinInputs(1);
+	SetMaxInputs(1);
 
 	// Parameters
-	/*SetParamInfof( FFPARAM_Roll, "Roll", FF_TYPE_STANDARD );
-	SetParamInfof( FFPARAM_Pitch, "Pitch", FF_TYPE_STANDARD );
-	SetParamInfof( FFPARAM_Yaw, "Yaw", FF_TYPE_STANDARD );*/
+	/*SetParamInfo(FFPARAM_BrightnessR, "R", FF_TYPE_RED, 0.5f);
+	m_BrightnessR = 0.5f;
+
+	/*SetParamInfo(FFPARAM_BrightnessG, "G", FF_TYPE_GREEN, 0.5f);
+	m_BrightnessG = 0.5f;
+
+	SetParamInfo(FFPARAM_BrightnessB, "B", FF_TYPE_BLUE, 0.5f);
+	m_BrightnessB = 0.5f;*/
+
 }
+
 EquiToFisheye::~EquiToFisheye()
 {
+
 }
 
-FFResult EquiToFisheye::InitGL( const FFGLViewportStruct* vp )
+FFResult EquiToFisheye::InitGL(const FFGLViewportStruct *vp)
 {
-	if( !shader.Compile( vertexShaderCode, fragmentShaderCode ) )
-	{
-		DeInitGL();
-		return FF_FAIL;
-	}
-	if( !quad.Initialise() )
-	{
-		DeInitGL();
-		return FF_FAIL;
-	}
 
-	//FFGL requires us to leave the context in a default state on return, so use this scoped binding to help us do that.
-	ScopedShaderBinding shaderBinding( shader.GetGLID() );
+	m_initResources = 0;
 
-	//We're never changing the sampler to use, instead during rendering we'll make sure that we're always
-	//binding the texture to sampler 0.
-	glUniform1i( shader.FindUniform( "inputTexture" ), 0 );
 
-	//We need to know these uniform locations because we need to set their value each frame.
-	maxUVLocation = shader.FindUniform( "MaxUV" );
-	fieldOfViewLocation = shader.FindUniform( "Brightness" );
+	//initialize gl shader
+	m_shader.Compile(vertexShaderCode, fragmentShaderCode);
 
-	//Use base-class init as success result so that it retains the viewport.
-	return CFreeFrameGLPlugin::InitGL( vp );
-}
-FFResult EquiToFisheye::ProcessOpenGL( ProcessOpenGLStruct* pGL )
-{
-	if( pGL->numInputTextures < 1 )
-		return FF_FAIL;
+	//activate our shader
+	m_shader.BindShader();
 
-	if( pGL->inputTextures[ 0 ] == NULL )
-		return FF_FAIL;
+	//to assign values to parameters in the shader, we have to lookup
+	//the "location" of each value.. then call one of the glUniform* methods
+	//to assign a value
+	m_inputTextureLocation = m_shader.FindUniform("InputTexture");
+	//m_BrightnessLocation = m_shader.FindUniform("brightness");
 
-	//FFGL requires us to leave the context in a default state on return, so use this scoped binding to help us do that.
-	ScopedShaderBinding shaderBinding( shader.GetGLID() );
+	//the 0 means that the 'inputTexture' in
+	//the shader will use the texture bound to GL texture unit 0
+	glUniform1i(m_inputTextureLocation, 0);
 
-	FFGLTextureStruct& Texture = *( pGL->inputTextures[ 0 ] );
-
-	//The input texture's dimension might change each frame and so might the content area.
-	//We're adopting the texture's maxUV using a uniform because that way we dont have to update our vertex buffer each frame.
-	FFGLTexCoords maxCoords = GetMaxGLTexCoords( Texture );
-	glUniform2f( maxUVLocation, maxCoords.s, maxCoords.t );
-
-	glUniform3f( fieldOfViewLocation,
-				 -1.0f + ( pitch * 2.0f ),
-				 -1.0f + ( yaw * 2.0f ),
-				 -1.0f + ( roll * 2.0f ) );
-
-	//The shader's sampler is always bound to sampler index 0 so that's where we need to bind the texture.
-	//Again, we're using the scoped bindings to help us keep the context in a default state.
-	ScopedSamplerActivation activateSampler( 0 );
-	Scoped2DTextureBinding textureBinding( Texture.Handle );
-
-	quad.Draw();
+	m_shader.UnbindShader();
 
 	return FF_SUCCESS;
 }
+
 FFResult EquiToFisheye::DeInitGL()
 {
-	shader.FreeGLResources();
-	quad.Release();
-	maxUVLocation = -1;
-	fieldOfViewLocation = -1;
+	m_shader.FreeGLResources();
+
 
 	return FF_SUCCESS;
 }
 
-FFResult EquiToFisheye::SetFloatParameter( unsigned int dwIndex, float value )
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Methods
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+FFResult EquiToFisheye::ProcessOpenGL(ProcessOpenGLStruct *pGL)
 {
-	switch( dwIndex )
+	if (pGL->numInputTextures < 1)
+		return FF_FAIL;
+
+	if (pGL->inputTextures[0] == NULL)
+		return FF_FAIL;
+
+	//activate our shader
+	m_shader.BindShader();
+
+	FFGLTextureStruct &Texture = *(pGL->inputTextures[0]);
+
+	//get the max s,t that correspond to the 
+	//width,height of the used portion of the allocated texture space
+	FFGLTexCoords maxCoords = GetMaxGLTexCoords(Texture);
+
+	//assign the Brightness
+	glUniform1f(m_BrightnessLocation,
+		-1.0f + (m_BrightnessR * 2.0f)/*,
+		-1.0f + (m_BrightnessG * 2.0f),
+		-1.0f + (m_BrightnessB * 2.0f)*/
+	);
+
+
+	//activate texture unit 1 and bind the input texture
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, Texture.Handle);
+
+	//draw the quad that will be painted by the shader/textures
+	//note that we are sending texture coordinates to texture unit 1..
+	//the vertex shader and fragment shader refer to this when querying for
+	//texture coordinates of the inputTexture
+	glBegin(GL_QUADS);
+
+	//lower left
+	glMultiTexCoord2f(GL_TEXTURE0, 0, 0);
+	glVertex2f(-1, -1);
+
+	//upper left
+	glMultiTexCoord2f(GL_TEXTURE0, 0, maxCoords.t);
+	glVertex2f(-1, 1);
+
+	//upper right
+	glMultiTexCoord2f(GL_TEXTURE0, maxCoords.s, maxCoords.t);
+	glVertex2f(1, 1);
+
+	//lower right
+	glMultiTexCoord2f(GL_TEXTURE0, maxCoords.s, 0);
+	glVertex2f(1, -1);
+	glEnd();
+
+	//unbind the input texture
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
+	//unbind the shader
+	m_shader.UnbindShader();
+
+	return FF_SUCCESS;
+}
+
+float EquiToFisheye::GetFloatParameter(unsigned int dwIndex)
+{
+	float retValue = 0.0;
+
+	switch (dwIndex)
 	{
-	/*case FFPARAM_Pitch:
-		aspectRatio = value;
-		break;
-	case FFPARAM_Yaw:
-		yaw = value;
-		break;
-	case FFPARAM_Roll:
-		fieldOfView = value;
-		break;*/
+		/*case FFPARAM_BrightnessR:
+			retValue = m_BrightnessR;
+			return retValue;
+		/*case FFPARAM_BrightnessG:
+			retValue = m_BrightnessG;
+			return retValue;
+		case FFPARAM_BrightnessB:
+			retValue = m_BrightnessB;
+			return retValue;*/
+	default:
+		return retValue;
+	}
+}
+
+FFResult EquiToFisheye::SetFloatParameter(unsigned int dwIndex, float value)
+{
+	switch (dwIndex)
+	{
+		/*case FFPARAM_BrightnessR:
+			m_BrightnessR = value;
+			break;
+		/*case FFPARAM_BrightnessG:
+			m_BrightnessG = value;
+			break;
+		case FFPARAM_BrightnessB:
+			m_BrightnessB = value;
+			break;*/
 	default:
 		return FF_FAIL;
 	}
 
 	return FF_SUCCESS;
-}
-
-float EquiToFisheye::GetFloatParameter( unsigned int dwIndex )
-{
-	switch( dwIndex )
-	{
-	/*case FFPARAM_Pitch:
-		return aspectRatio;
-	case FFPARAM_Yaw:
-		return yaw;
-	case FFPARAM_Roll:
-		return fieldOfView;*/
-
-	default:
-		return 0.0f;
-	}
 }
--- source/EquiToFisheye/EquiToFisheye.h
+++ source/EquiToFisheye/EquiToFisheye.h
@@ -1,8 +1,8 @@
 #pragma once
+
+#include <FFGLShader.h>
+#include "FFGLPluginSDK.h"
 #include <string>
-#include <ffgl/FFGLPluginSDK.h>
-#include <ffglex/FFGLShader.h>
-#include <ffglex/FFGLScreenQuad.h>
 
 class EquiToFisheye : public CFreeFrameGLPlugin
 {
@@ -10,22 +10,38 @@ public:
 	EquiToFisheye();
 	~EquiToFisheye();
 
-	//CFreeFrameGLPlugin
-	FFResult InitGL( const FFGLViewportStruct* vp ) override;
-	FFResult ProcessOpenGL( ProcessOpenGLStruct* pGL ) override;
+	///////////////////////////////////////////////////
+	// FreeFrame plugin methods
+	///////////////////////////////////////////////////
+
+	FFResult SetFloatParameter(unsigned int dwIndex, float value) override;
+	float GetFloatParameter(unsigned int index) override;
+	FFResult ProcessOpenGL(ProcessOpenGLStruct* pGL) override;
+	FFResult InitGL(const FFGLViewportStruct *vp) override;
 	FFResult DeInitGL() override;
 
-	FFResult SetFloatParameter( unsigned int dwIndex, float value ) override;
+	///////////////////////////////////////////////////
+	// Factory method
+	///////////////////////////////////////////////////
 
-	float GetFloatParameter( unsigned int index ) override;
+	static FFResult __stdcall CreateInstance(CFreeFrameGLPlugin **ppOutInstance)
+	{
+		*ppOutInstance = new EquiToFisheye();
+		if (*ppOutInstance != NULL)
+			return FF_SUCCESS;
+		return FF_FAIL;
+	}
 
-private:
-	float pitch;
-	float yaw;
-	float roll;
 
-	ffglex::FFGLShader shader;   //!< Utility to help us compile and link some shaders into a program.
-	ffglex::FFGLScreenQuad quad; //!< Utility to help us render a full screen quad.
-	GLint maxUVLocation;
-	GLint fieldOfViewLocation;
+protected:
+	// Parameters
+	float m_BrightnessR;
+	//float m_BrightnessG;
+	//float m_BrightnessB;
+	int m_initResources;
+
+	FFGLShader m_shader;
+	GLint m_inputTextureLocation;
+	GLint m_BrightnessLocation;
+
 };
